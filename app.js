4d835a57039cbbe9ea4d44550b72db4022a84966
admin42045 <nitinkumar42045@gmail.com>
2021-08-18 15:41:26 +0530

    add create page

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.get("/contact", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public/contact.html"));
 });
 
+app.get("/create", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/create.html"));
+});
+
+app.post("/post/store", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 app.listen(port, () => {
   console.log(`Server is loading on ... http://${port}/`);
 });
