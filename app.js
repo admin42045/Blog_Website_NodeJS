3d32c87fca620ea06325f0aec31dc47135eec6e1
admin42045 <nitinkumar42045@gmail.com>
2021-08-18 12:59:51 +0530

    Initial commit (frontend add )

---
+++ app.js
@@ -0,0 +1,29 @@
+// createing first server
+const express = require("express");
+
+const path = require("path");
+
+const app = new express();
+
+const port = process.env.PORT || 5050;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/about.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/post.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/contact.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is loading on ... http://${port}/`);
+});
